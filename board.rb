43a61fa72061d332366b12e314d7f35ce3fdefdf
Allison <allisonmariebrowne@gmail.com>
2015-01-31 13:50:30 -0500

    it checks who wins, computer needs to make moves

--- board.rb
+++ board.rb
@@ -2,7 +2,7 @@
 ## Unbeatable Tic Tac Toe in Ruby
 
 class Board
-  attr_accessor :squares, :board_description
+  attr_accessor :squares, :select_desc, :status, :winning_combos
 
   def initialize
     @squares = {
@@ -11,15 +11,24 @@ class Board
       c1: "-", c2: "-", c3: "-"
     }
 
-    @board_description = "a1 | a2 | a3
-      ------------
-      b1 | b2 | b3
-      ------------
-      c1 | c2 | c3"
+    @select_desc =
+      "a1 | a2 | a3 \n" +
+      "------------ \n" +
+      "b1 | b2 | b3 \n" +
+      "------------ \n" +
+      "c1 | c2 | c3 \n"
   end
 
-  def winning_combinations
-    @combos = [
+  def status
+    "#{@squares[:a1]} | #{@squares[:a2]} | #{@squares[:a3]} \n" +
+    "------------ \n" +
+    "#{@squares[:b1]} | #{@squares[:b2]} | #{@squares[:b3]} \n" +
+    "------------ \n" +
+    "#{@squares[:c1]} | #{@squares[:c2]} | #{@squares[:c3]} \n"
+  end
+
+  def winning_combos
+    [
       [:a1, :a2, :a3], # 1
       [:b1, :b2, :b3], # 2
       [:c1, :c2, :c3], # 3
@@ -27,17 +36,19 @@ class Board
       [:a2, :b2, :c2], # 5
       [:a3, :b3, :c3], # 6
       [:a1, :b2, :c3], # 7
-      [:a1, :b2, :c3]  # 8
+      [:a3, :b2, :c1]  # 8
     ]
   end
 
   def change_square(position, player)
     if @squares[position] == "-" && player == :computer
-      @squares[position] = "x"
-    elsif @squares[position] == "-" && player == :human
       @squares[position] = "o"
+    elsif @squares[position] == "-" && player == :human
+      @squares[position] = "x"
     else
       return :occupied
     end
   end
+
+
 end
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,14 +1,49 @@
-# PLAY unbeatable command line tic tac toe
+## Playing logic
+## Unbeatable Tic Tac Toe in Ruby
 
 require 'pry'
 require_relative 'board'
 
 @board = Board.new
-puts "This is your board"
-puts @board_description
+puts "Use these combinations to select from the board"
+puts @board.select_desc
 
-puts "where whould you like to put your X?"
+def player_goes
+  puts "where whould you like to put your X?"
+  position = gets.chomp.to_sym
+  @board.change_square(position, :human)
+  puts @board.status
+end
+
+def check_win
+  # don't need to write this because they will never win!! >:P
+  #  will write for testing purposes
+  results = []
+  @board.winning_combos.each do |combo|
+    combo.each do |square_key|
+      results << @board.squares[square_key]
+      if results == ["x", "x", "x"]
+        return true
+        break
+      elsif results == ["o", "o", "o"]
+        return false
+        break
+      end
+    end
+    results = []
+  end
+end
+
+loop do
+  player_goes
+  #cumputer_goes
+  if check_win == true
+    puts "you win"
+    break
+  elsif check_win == false
+    puts "you loose"
+    break
+  end
+end
 
 
- @board.change_square(:a1, :computer)
-  puts @board.squares
